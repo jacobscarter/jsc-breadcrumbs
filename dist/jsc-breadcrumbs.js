85fbceda77623f33a787d3fefe865b6c2792fbf9
jacobscarter <jacob@ieksolutions.com>
2015-01-07 15:11:23 -0700

    Product Build

--- dist/jsc-breadcrumbs.js
+++ dist/jsc-breadcrumbs.js
@@ -1,18 +1,9 @@
 /**
  * Easy to use breadcrumbs for AngularJS
- * @version v1.0.1 - 2015-01-07 * @link https://github.com/jacobscarter/jsc-breadcrumbs
+ * @version v1.1.0 - 2015-01-07 * @link https://github.com/jacobscarter/jsc-breadcrumbs
  * @author Jacob Carter <jacob@ieksolutions.com>
  * @license MIT License, http://www.opensource.org/licenses/MIT
  */
-angular.module('templates-jscBreadcrumbs', ['template.html']);
-
-angular.module("template.html", []).run(["$templateCache", function($templateCache) {
-  $templateCache.put("template.html",
-    "<div class=\"back-button\" ng-hide=\"hide\" ng-click=\"backButton()\">\n" +
-    "	<a>BACK</a>\n" +
-    "</div>");
-}]);
-
 var jscBreadcrumbs = angular.module('jscBreadcrumbs', ['templates-jscBreadcrumbs']);
 
 jscBreadcrumbs.directive('jscBreadcrumbs', ['$rootScope', '$log', '$state', '$stateParams', 'JSCBreadcrumbs',
@@ -20,16 +11,29 @@ jscBreadcrumbs.directive('jscBreadcrumbs', ['$rootScope', '$log', '$state', '$st
 
         return {
             restrict: 'E',
-            scope: true,
+            scope: {
+                backbutton : "=",
+                breadcrumbtrail : "="
+            },
             replace: true,
             templateUrl: 'template.html',
             link: function ($scope, $element, $attrs) {
 
+                $scope.backButtonShow = false;
+                $scope.breadcrumbTrailShow = false;
+
+                if($scope.backbutton){
+                    $scope.backButtonShow = true;
+                }
+                if($scope.breadcrumbtrail){
+                    $scope.breadcrumbTrailShow = true;
+                }
+
                 var breadcrumbs;
-                //list of state that trigger breadcrumb reset when you enter another state from them
-                var resetArray = [];
+                $scope.trailArray = [];
                 //this stores the state you just left via backButton
                 var backButtonState
+
 
 
                 //making sure the breadcrumbs array in directive scope is up to date with the breadcrumbs stored
@@ -55,16 +59,11 @@ jscBreadcrumbs.directive('jscBreadcrumbs', ['$rootScope', '$log', '$state', '$st
                     }
 
 
-                    //resetting breadcrumb if fromState matches reset state
-                    for(var i in resetArray){
-                        if(fromState.name === resetArray[i]){
-                            breadcrumbs.length = 0;
-                        }
-                    }
+                    
 
 
                     //hide breadcrumbs logic
-                    if(toParams.breadcrumbs && toParams.breadcrumbs.doNotShow === true){
+                    if(toState.data.breadcrumbs && toState.data.breadcrumbs.doNotShow === true){
                         JSCBreadcrumbs.breadcrumbData.hide = true;
                         $scope.hide = JSCBreadcrumbs.breadcrumbData.hide;
                     } else {
@@ -72,13 +71,17 @@ jscBreadcrumbs.directive('jscBreadcrumbs', ['$rootScope', '$log', '$state', '$st
                         $scope.hide = JSCBreadcrumbs.breadcrumbData.hide;
                     }
 
-                    //I have seperated the below into several if statements so the truthy checks aren't
-                    //impossible to understand
 
                     //check to stop duplicate adding of states for multiple loading of directive
                     //this is why I hate digest cycle sometimes...
+                    if(fromState.data && fromState.data.breadcrumbs && fromState.data.breadcrumbs.name){
+                        var breadcrumbName = fromState.data.breadcrumbs.name;
+                    } else {
+                        var breadcrumbName = fromState.name;
+                    }
                     if(fromState.name.length > 0){
                         breadcrumbs.push({
+                            name : breadcrumbName,
                             state : fromState,
                             params : fromParams
                         });
@@ -89,13 +92,17 @@ jscBreadcrumbs.directive('jscBreadcrumbs', ['$rootScope', '$log', '$state', '$st
 
                     //check stateParam to see if state should not be stored
                     //this is one of the last steps done!
-                    if(fromParams.breadcrumbs && fromParams.breadcrumbs.doNotStore){
+                    if(fromState.data && fromState.data.breadcrumbs && fromState.data.breadcrumbs.doNotStore){
                         $scope.breadcrumbPop();
                     }
 
-                    
+                    //resetting breadcrumb if fromState matches reset state
+                    if(toState.data && toState.data.breadcrumbs && toState.data.breadcrumbs.reset){
+                        breadcrumbs.length = 0;
+                    }
                 
                     sessionStorage.setItem('jscBreadcrumbs', JSON.stringify(breadcrumbs));
+                    $scope.trailArray = breadcrumbs;
 
 
                     //if breadcrumbs array is empty we wont show breadcrumbs
@@ -103,6 +110,9 @@ jscBreadcrumbs.directive('jscBreadcrumbs', ['$rootScope', '$log', '$state', '$st
                         JSCBreadcrumbs.breadcrumbData.hide = true;
                         $scope.hide = JSCBreadcrumbs.breadcrumbData.hide;
                     }
+
+                    
+
 
                     
 
@@ -137,8 +147,17 @@ jscBreadcrumbs.directive('jscBreadcrumbs', ['$rootScope', '$log', '$state', '$st
                         $scope.hide = JSCBreadcrumbs.breadcrumbData.hide = true;
                         sessionStorage.removeItem('jscBreadcrumbs');
                     }
+                };
 
-                    
+                $scope.breadcrumbClick = function(index) {
+                    $state.go(breadcrumbs[index].state.name, breadcrumbs[index].params);
+                    breadcrumbs.splice(breadcrumbs.length -2, 2);
+                    sessionStorage.setItem('jscBreadcrumbs', JSON.stringify(breadcrumbs));
+                    if(breadcrumbs.length === 0){
+                        JSCBreadcrumbs.breadcrumbData.hide = true;
+                        $scope.hide = JSCBreadcrumbs.breadcrumbData.hide = true;
+                        sessionStorage.removeItem('jscBreadcrumbs');
+                    }
                 };
                 //end Breadcrumbs
 
@@ -153,8 +172,15 @@ angular.module('templates-jscBreadcrumbs', ['template.html']);
 
 angular.module("template.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("template.html",
-    "<div class=\"back-button\" ng-hide=\"hide\" ng-click=\"backButton()\">\n" +
-    "	<a>BACK</a>\n" +
+    "<div class=\"jscbreadcrumbs\">\n" +
+    "	<div class=\"back-button\" ng-hide=\"hide\" ng-click=\"backButton()\" ng-if=\"backButtonShow\">\n" +
+    "		<a>BACK</a>\n" +
+    "	</div>\n" +
+    "	<div class=\"breadcrumb-trail\" ng-if=\"breadcrumbTrailShow\" ng-hide=\"hide\">\n" +
+    "		<ul>\n" +
+    "			<li ng-repeat=\"item in trailArray\" ng-click=\"breadcrumbClick($index)\">{{item.name}} /</li>\n" +
+    "		</ul>\n" +
+    "	</div>\n" +
     "</div>");
 }]);
 
@@ -164,7 +190,6 @@ jscBreadcrumbs.factory('JSCBreadcrumbs', ['$rootScope', '$state', '$log', '$time
 
 
 	//breadcrumbs
-	
 	var breadcrumbsArray = [];
 	var breadcrumbData = {};
 	breadcrumbData.hide = false;
