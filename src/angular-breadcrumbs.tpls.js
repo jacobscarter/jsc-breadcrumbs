0565dfced16e9c47c14f0499725e2d27ffa6e5a5
jacobscarter <jacob@ieksolutions.com>
2015-01-07 15:12:09 -0700

    Release 1.1.0

--- src/angular-breadcrumbs.tpls.js
+++
@@ -1,8 +0,0 @@
-angular.module('templates-jscBreadcrumbs', ['template.html']);
-
-angular.module("template.html", []).run(["$templateCache", function($templateCache) {
-  $templateCache.put("template.html",
-    "<div class=\"back-button\" ng-hide=\"hide\" ng-click=\"backButton()\">\n" +
-    "	<a>BACK</a>\n" +
-    "</div>");
-}]);
